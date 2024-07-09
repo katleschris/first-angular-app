257d0bbe35f293d9051ab919e7a727425711a508
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2024-07-09 12:23:05 +0200

    Used inputs to display User data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [HeaderComponent, UserComponent]
 })
 export class AppComponent {
   title = 'first-angular-app';
+  users = DUMMY_USERS;
 }
